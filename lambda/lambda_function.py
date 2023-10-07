abaa3fe66245f26054a39d9b6b3ac77352663be7
Matthias Lau <lau.matthias@gmail.com>
2023-10-07 20:02:01 +0200

    Adjust to my personal german use case

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,20 +1,26 @@
 import logging
 import ask_sdk_core.utils as ask_utils
 import openai
+import os
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Set your OpenAI API key
-openai.api_key = "PUT YOUR OPENAI API KEY HERE"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
 
@@ -22,20 +28,22 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Chat G.P.T. mode activated"
+        speak_output = "Neugierig?"
 
         session_attr = handler_input.attributes_manager.session_attributes
         session_attr["chat_history"] = []
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
         )
+
 
 class GptQueryIntentHandler(AbstractRequestHandler):
     """Handler for Gpt Query Intent."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return ask_utils.is_intent_name("GptQueryIntent")(handler_input)
@@ -50,14 +58,16 @@ class GptQueryIntentHandler(AbstractRequestHandler):
         session_attr["chat_history"].append((query, response))
 
         return (
-                handler_input.response_builder
-                    .speak(response)
-                    .ask("Any other questions?")
-                    .response
-            )
+            handler_input.response_builder
+            .speak(response)
+            .ask("Nochwas?")
+            .response
+        )
+
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Generic error handling to capture any syntax or routing errors."""
+
     def can_handle(self, handler_input, exception):
         # type: (HandlerInput, Exception) -> bool
         return True
@@ -66,17 +76,19 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
         # type: (HandlerInput, Exception) -> Response
         logger.error(exception, exc_info=True)
 
-        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+        speak_output = "Upsi, das hat nicht geklappt."
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
         )
+
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
@@ -84,32 +96,34 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Leaving Chat G.P.T. mode"
+        speak_output = "Tschüssi."
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .response
+            .speak(speak_output)
+            .response
         )
+
 
 def generate_gpt_response(chat_history, new_question):
     try:
-        messages = [{"role": "system", "content": "You are a helpful assistant."}]
+        messages = [{"role": "system", "content": "You are a helpful home assistant, explaining the world to a child. Do not make things up, say you don't know if you are unsure. Answer in german."}]
         for question, answer in chat_history[-10:]:
             messages.append({"role": "user", "content": question})
             messages.append({"role": "assistant", "content": answer})
         messages.append({"role": "user", "content": new_question})
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
             messages=messages,
             max_tokens=100,
             n=1,
             stop=None,
-            temperature=0.5
+            temperature=0.7
         )
         return response['choices'][0]['message']['content'].strip()
     except Exception as e:
         return f"Error generating response: {str(e)}"
+
 
 sb = SkillBuilder()
 
